47c69f37d71262034643df4b2316477f62096d82
Salvatore Di Lorenzo <salvadil02@gmail.com>
2024-05-21 12:55:02 +0200

    aggiunto gitignore

--- software/prova.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    char *str = "ciao";
-    int a = 5;
-    cout<<a + *str;
-}
--- web/php/redirect.php
+++
@@ -1,4 +0,0 @@
-<?php
-	header('Location: http://127.0.0.1/get_flag.php');
-	header('Content-Type:image/png');
-?>
